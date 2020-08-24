b2fc6f6e0efdedc4733559de48b2579a0352f97f
yuki <syuoo0025@gmail.com>
2020-08-24 21:59:14 +0900

    ~2:14 書き方変更

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,16 +1,9 @@
 <template>
   <div>
     <h1>Cats for Adoption</h1>
-    <!-- <b-table striped hover :items="cats"> -->
-        <!-- A custom formatted column -->
-        <!-- <template slot="name" slot-scope="data"> -->
-
-
     <!-- 現在バージョン -->
     <b-table :fields="fields" :items="cats">
       <template v-slot:cell(name)="data">
-
-
           <router-Link :to="`/pets/${data.value}`">
           {{ data.value }}</router-Link>
         </template>
@@ -19,13 +12,17 @@
 </template>
 
 <script>
-import cats from '@/data/cats'
+import { mapState } from 'vuex'
 
 export default {
   data() {
-    return {
-      cats
-    }
+    return {}
+  },
+
+  computed: {
+    ...mapState([
+      'cats'
+    ])
   }
 }
 </script>
